46ec3d80edb1410f2481e0bc9e5992cb29fbd495
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-13 16:59:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const sourceArray = [1, 2, 3, -9];
 
-
-function map(sourceArray, fn) {
-  let arr = []
-
-  for (let i = 0; i < sourceArray.length; i++) {
-    let theElement = sourceArray[i]
-    arr.push(fn(theElement))
+function map(sourceArray,fun) {
+  let arr=[];
+  for (var i = 0; i < sourceArray.length; i++) {
+    let element = sourceArray[i];
+    arr.push(fun(element));
   }
-return arr;
+  return arr;
 }
+
+console.log(sourceArray,function(element) {
+      return -1 * element;
+  });
